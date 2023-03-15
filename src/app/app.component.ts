3687afbbe5934356d4fec321415dbf38b0e8af40
Raul Olvera <raulolverat22@hotmail.com>
2023-03-15 14:10:52 -0700

    app con interpolacion del texto, falta interpolar titulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'advice-generator';
+
+  advice!:string;
+
+  constructor(private http:HttpClient){}
+
+  ngOnInit(){
+    this.http.get<any> ('https://api.adviceslip.com/advice').subscribe(data => {
+
+      this.advice = data.slip.advice;
+
+    });
+  }
+
 }
